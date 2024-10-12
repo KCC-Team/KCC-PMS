33746309ec89232971a04e530158247d022b608b
SoohoLee <hyeri0603@naver.com>
2024-10-13 00:01:55 +0900

    feat: 결함 관리 페이지 UI, 로직 구현 (#55)

---
+++ src/main/java/com/kcc/pms/domain/task/defect/controller/DefectController.java
@@ -0,0 +1,24 @@
+package com.kcc.pms.domain.task.defect.controller;
+
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@NoArgsConstructor
+@RequestMapping("/projects/{projectId}/defects")
+public class DefectController {
+
+    @GetMapping
+    public String findAll(){
+        return "defect/list";
+    }
+
+    @GetMapping("/{id}")
+    public String findById(Model model, @PathVariable Long id) {
+        return "defect/defect";
+    }
+}
