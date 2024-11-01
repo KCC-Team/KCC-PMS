9a0d7d9c95bfdea2ee3a13268de97778fd587167
SoohoLee <hyeri0603@naver.com>
2024-11-01 14:11:05 +0900

    refactor: 더미 경로 추가 (#234)

--- src/main/java/com/kcc/pms/domain/common/controller/CommonController.java
+++ src/main/java/com/kcc/pms/domain/common/controller/CommonController.java
@@ -1,6 +1,7 @@
 package com.kcc.pms.domain.common.controller;
 
 import com.kcc.pms.auth.PrincipalDetail;
+import com.kcc.pms.domain.common.config.EnvVariableProperties;
 import com.kcc.pms.domain.common.model.dto.CommonCodeSelectListResponseDto;
 import com.kcc.pms.domain.common.service.CommonService;
 import com.kcc.pms.domain.project.model.dto.ProjectManagerResponseDto;
@@ -9,20 +10,26 @@ import com.kcc.pms.domain.project.service.ProjectService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.net.URI;
 import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
 public class CommonController {
-
     private final CommonService commonService;
     private final ProjectService projectService;
+
+    private final EnvVariableProperties properties;
 
     @GetMapping({"/projects", "/outputs"})
     @ResponseBody
@@ -69,19 +76,6 @@ public class CommonController {
         return commonService.getCommonCodeSelectList(commonCodeNo);
     }
 
-    // 파일 업로드 테스트 API
-    @PostMapping("/fileUpload")
-    @ResponseBody
-    public ResponseEntity<Void> fileUpload(HttpSession session,
-                                           @AuthenticationPrincipal PrincipalDetail principalDetail,
-                                           @RequestPart("files") List<MultipartFile> files,
-                                           @RequestParam(value = "flcd", required = false) String fl_cd) {
-//        Long prjNo = (Long) session.getAttribute("prjNo");
-        Long prjNo = 1L;
-        commonService.fileUpload(files, principalDetail.getMember().getMemberName(), prjNo, fl_cd);
-        return ResponseEntity.ok().build();
-    }
-
     @GetMapping("/fileDownload")
     @ResponseBody
     public ResponseEntity<byte[]> fileDownload(@RequestParam("filePath") String filePath) {
@@ -91,4 +85,29 @@ public class CommonController {
             return ResponseEntity.badRequest().build();
         }
     }
+
+    @GetMapping("/files-kccpms/{projectNo}/{fileName:.+}")
+    public ResponseEntity<Void> getImage(
+            @PathVariable("projectNo") String userId,
+            @PathVariable("fileName") String fileName) {
+        try {
+            if (!isValidFileName(fileName)) {
+                return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+            }
+
+            String path = properties.getS3().getUrl() + userId + "/" + fileName;
+
+            HttpHeaders headers = new HttpHeaders();
+            headers.setLocation(URI.create(path));
+            return new ResponseEntity<>(headers, HttpStatus.FOUND);
+
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    private boolean isValidFileName(String fileName) {
+        return !fileName.contains("..") && !fileName.contains("/");
+    }
+
 }
